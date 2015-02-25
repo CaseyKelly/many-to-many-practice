4291942313e4ff5c7c6c09c22c1b709f4d74e635
CaseyKelly <casey.kelly@colorado.edu>
2015-02-25 14:35:51 -0700

    Add validations

--- app/models/company_review.rb
+++ app/models/company_review.rb
@@ -1,4 +1,6 @@
 class CompanyReview < ActiveRecord::Base
   belongs_to :user
-  belongs_to :company  
+  belongs_to :company
+
+  validates :title, :description, :company_id, :user_id, :rating, presence: true
 end
--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -2,4 +2,5 @@ class ProductReview < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
 
+  validates :title, :description, :product_id, :user_id, :rating, presence: true
 end
